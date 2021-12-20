e1361356dc51d3c07d315565a8e1f892ea72e5e0
Najib <muhammed.najib.farah@gmail.com>
2021-12-20 10:30:25 +0000

    days changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,28 +23,42 @@
       handle: "@rd",
     },
     content: {
-      text: "Je pense , donc je suis",
+        text: "Je pense , donc je suis",
     },
     created_at: 1461113959088,
-  },
+},
 ]; */
 
 $(document).ready(function () {
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    }).then((response) => {
+      //   console.log("RESPONSE: ");
+      //   console.log(response);
+      renderTweets(response);
+      $(".counter").text(140);
+      $("#tweet-text".val(""));
+    });
+  };
+  loadTweets();
+  $("#errorMessage").hide();
+
   const renderTweets = function (tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
-    for (let user of tweets) {
-      const $tweet = createTweetElement(user);
-      $(".tweet-container").prepend($tweet);
+    $(".tweets-container").empty();
+    for (let tweet of tweets) {
+      $(".tweets-container").prepend(createTweetElement(tweet));
     }
   };
-  renderTweets(data);
 
   const createTweetElement = function (tweet) {
     let date = new Date(tweet.created_at).toLocaleDateString();
-
-    return `
+    let $tweet = $(
+      `
     <article class="tweet">
     <header>
     <div class="info">
@@ -62,26 +76,39 @@ $(document).ready(function () {
     <span id="flag" class="fas fa-flag"></span>
     </footer>
     </article>
-    `;
+    `
+    );
+    return $tweet;
   };
 
-  const $newTweet = $("#submit-tweet");
+  const $newTweet = $("#tweet-form");
   $newTweet.on("submit", function (event) {
     event.preventDefault();
+    const tweet = $("#tweet-text").val().trim().length;
+    if (!tweet) {
+      $("errorMessage").show();
+      $("#errorMessage").text("Tweet cannot be empty!");
+    }
+
+    if (tweet > 140) {
+      $("errorMessage").show();
+      $("#errorMessage").text("Tweet can't be longer than 140 characters!");
+    } else {
+      const val = $(this).serialize();
+      $.ajax("/tweets", {
+        method: "POST",
+        data: val,
+      }).then(() => {
+        $("#errorMessage").hide();
+        loadTweets();
+        $("#tweet-text").val("");
+      });
+    }
   });
 
-  const loadTweets = function () {
-    $.ajax({
-      method: "GET",
-      url: "/tweets",
-    }).then((response) => {
-      console.log("RESPONSE: ");
-      console.log(response);
-      renderTweets(response);
-      // $('#tweet-text').val("");
-      $(".counter").text(140);
-    });
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   };
-  loadTweets();
-  $("#errorMessage").hide();
 });
