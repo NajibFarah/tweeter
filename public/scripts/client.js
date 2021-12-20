bae54bc2e89e9649ecfe760645155aefff83ae12
Najib <muhammed.najib.farah@gmail.com>
2021-12-20 03:35:25 +0000

    load tweets & pther stuff edited

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,10 @@
-const data = [
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+
+/* const data = [
   {
     user: {
       name: "Newton",
@@ -21,29 +27,61 @@ const data = [
     },
     created_at: 1461113959088,
   },
-];
-const createTweetElement = function (info) {
-  const date = new Date(tweet.created_at).toLocaleDateString();
-  return `
-  <article class="tweet-header wrapper">
-  <header>
-    <!-- <div class="left">             -->
-    <i class="fas fa-user-alt"></i>
-    <h1 class="left">EdenHazard</h1>
-    <!-- </div> -->
-    <h5 class="right">@EdenHazard</h5>
-  </header>
-  <p class="message">Up the Chelssss #KTBFFH ⚡️</p>
-  <footer class="tweet-footer">
-    <div>
-      <p class="left" id="days">10 Days Ago</p>
-    </div>
+]; */
 
-    <div>
-      <span class="fas fa-flag"></span>
-      <span class="fas fa-retweet"></span>
-      <span class="fas fa-heart"></span>
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let user of tweets) {
+      const $tweet = createTweetElement(user);
+      $(".tweet-container").prepend($tweet);
+    }
+  };
+  renderTweets(data);
+
+  const createTweetElement = function (tweet) {
+    let date = new Date(tweet.created_at).toLocaleDateString();
+
+    return `
+    <article class="tweet">
+    <header>
+    <div class="info">
+    <img src=${tweet.user.avatars} class="avatar"/>
+    <span class="name">${tweet.user.name}</span>
     </div>
-  </footer>
-</article> `;
-};
+    <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <p class="tweetData">${escape(tweet.content.text)}</p>
+    <footer>
+    <span class="date">${date}</span>
+    <span class ="icons">
+    <span id="heart" class="fas fa-heart"></span>
+    <span id="retweet" class="fas fa-retweet"></span>
+    <span id="flag" class="fas fa-flag"></span>
+    </footer>
+    </article>
+    `;
+  };
+
+  const $newTweet = $("#submit-tweet");
+  $newTweet.on("submit", function (event) {
+    event.preventDefault();
+  });
+
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    }).then((response) => {
+      console.log("RESPONSE: ");
+      console.log(response);
+      renderTweets(response);
+      // $('#tweet-text').val("");
+      $(".counter").text(140);
+    });
+  };
+  loadTweets();
+  $("#errorMessage").hide();
+});
